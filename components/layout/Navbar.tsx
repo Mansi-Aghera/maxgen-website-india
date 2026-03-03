0a4f17813a4d5ba1e8c370039658e387dd2f7054
Mansi-Aghera <mansi@codingcloudinstitude.com>
2026-03-03 16:12:48 +0530

    now navbar

--- components/layout/Navbar.tsx
+++ components/layout/Navbar.tsx
@@ -17,7 +17,7 @@ const navItems = [
   { label: "Career", href: "/career", dropdown: "career" }, // updated
   { label: "About", href: "/about" },
   { label: "Resources", href: "/blogs", dropdown: "resources" },
-  { label: "Industry", href: "/industry",},
+  // { label: "Industry", href: "/industry",},
 
 ];
 
